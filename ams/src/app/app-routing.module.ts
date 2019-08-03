9b17ae6531b94ca7a91760bf83a34286975f6a17
zkhan <zkhan@webmd.net>
2019-08-03 12:31:23 +0530

    Package changes

--- ams/src/app/app-routing.module.ts
+++ ams/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DisplayComponent } from './display/display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:DisplayComponent,
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
